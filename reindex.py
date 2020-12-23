79965ab11b146f2cd41150e49593ff597852788d
daesoo <dskim9107@gmail.com>
2020-12-23 23:37:48 +0900

    add match query

--- reindex.py
+++ reindex.py
@@ -1,2 +1,14 @@
+import pandas as pd
 from elasticsearch import Elasticsearch, helpers
+
+def bulk_index() :
+
+    data = pd.read_csv('ssg.csv')
+    document = data.to_dict("records")
+    es = Elasticsearch(hosts="localhost:9200")
+    helpers.bulk(es, document, index='ssg_item_lv1')
+
+
+
+
 
