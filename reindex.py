60a8cf543e8e1613c521748b4ea449cfb4671f9d
daesoo <dskim9107@gmail.com>
2020-12-24 00:14:17 +0900

    add index settings

--- reindex.py
+++ reindex.py
@@ -9,6 +9,17 @@ def bulk_index() :
     helpers.bulk(es, document, index='ssg_item_lv1')
 
 
+def index() :
+    data = pd.read_csv('ssg.csv')
+    data['ssg_price'] = data['ssg_price'].apply(lambda x : x.replace(",", ""))
+    data['ssg_price'] = data['ssg_price'].astype(int)
+    document = data.to_dict("records")
+    settings = open('config/settings.json').read()
+    es = Elasticsearch(hosts="localhost:9200")
+    es.indices.create(index='ssg_item_lv1', body=settings)
+    helpers.bulk(es, document, index='ssg_item_lv1')
+
+
 
 
 
